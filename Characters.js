1c89cba53e7ff88276cba0fe501a5d3ab857f724
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-16 17:09:19 +0200

    Delete Characters.js

--- Characters.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const characterSchema = new mongoose.Schema({
-    name: { type: String, required: true }
-});
-
-const Character = mongoose.model("Character", characterSchema);
-
-module.exports = Character;
